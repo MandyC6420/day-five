c2380d4e45597cb36d72f84e3372967d8020217b
Mandy <drgnfli6420@yahoo.com>
2020-03-22 18:00:33 -0400

    added practice work morehelp.html

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
 var favoriteFood = "friedpickles";
 if (favoriteFood === "friedpickles" ||favoriteFood === "pepperoni rolls")
 console.log ("congrats you have excellent taste");
+
+//Use JavaScript to check and see if the following comparisons are true. For each of the following, log "True fact!" to the console if the condition evaluates to true and "LIES!" to the console if it doesn't.
+//Is the sum of 1 and 1 greater than 5?
+//Is the product of 4 and 5 less than our equal to 20?
+//Is the difference between 6 and 2 greater than or equal to 0?
+
+//3 > 23 // false, 3 is not greater than 23
+//22 < 100 // true, 22 is less than 100
+//24 <= 25 // true, 24 is less than or equal to 25
+//62 >= 4 // true, 62 is greater than or equal to 4
+
+1 > 5 // false, 2 is not greater than 5
+
+
+//In your JavaScript file, write a while loop that outputs the phrase “TWEET TWEET” 100 times to the console.
+
+
+var phrase = 1
+while(phrase  <=100){
+    console.log ("Tweet Tweet")
+    phrase = phrase + 1
+}
+
+if (2 + 2 === 5) {
+    console.log("Math Works!");
+} else {
+    console.log("Math is broken.");
+}
+
+var likesPizza = false;
+
+console.log(likesPizza)
+
+if(likesPizza === true){
+    console.log("Have some pizza!")
+}
+
+var favoriteFood = "Pizza";
+
+if(favoriteFood === "tacos"){
+    console.log("Heck yeah! You have great taste!")
+} else if (favoriteFood === "pizza") {
+    console.log("You have pretty good taste but have you tried tacos?")
+} else {
+    console.log("You have terrible taste.  Please eat a taco")
+}
+
+var time ="afternoon";
+
+if (time === "morning"){
+    console.log("Good Morning!");
+    } else if (time === "afternoon"){
+        console.log("Good afternoon!");
+    }else if (time === "evening"){
+        console.log("Good evening!");
+    }else {
+        console.log("Hello");
+    }
+
+    
